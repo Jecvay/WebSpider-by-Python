5a984eba25f1e0a23617356a31884ad9989b1f27
Jecvay <ljwjiewei@gmail.com>
2016-10-09 21:18:18 +0800

    loginManager done

--- login_manager.py
+++ login_manager.py
@@ -1,24 +1,49 @@
 # coding=utf-8
 
 from login_manager_base import LoginManagerBase
+from utils.singleton import singleton
+import requests
+import config
 
 
+@singleton
 class ZhihuLoginManager(LoginManagerBase):
+    """
+        登录之后, 就可以获取 session 来进行网络操作
+    """
 
-	def __init__(self):
-		super().__init__()
-		self.url_root = 'www.zhihu.com'
-		self.url_start = ''
-		self.set_login_method('cookie')
+    def __init__(self):
+        super().__init__()
+        self.url_root = 'www.zhihu.com'
+        self.url_start = ''
+        self.cookie = {}
+        self._session = None
 
-	def _login_by_post(self):
-		pass
+    @property
+    def session(self):
+        return self._session
 
-	def _login_by_cookie(self):
-		print ("login_by_cookie !!")
+    def _login_by_post(self):
+        pass
+
+    def _login_by_cookie(self):
+        """
+            关于知乎 cookie 的说明:
+                1. 在浏览器控制台中执行 console.log(document.cookie)
+                2. 找到 z_c0 & login 两部分复制过来
+        """
+        print("login_by_cookie !!")
+        self.cookie['login'] = config.ZHIHU_COOKIE_LOGIN
+        self.cookie['z_c0'] = config.ZHIHU_COOKIE_Z0
+        cookie_jar = requests.utils.cookiejar_from_dict(self.cookie)
+        self._session = requests.Session()
+        self._session.headers['User-Agent'] = config.USER_AGENT
+        self._session.cookies = cookie_jar
 
 
 if __name__ == '__main__':
-	login_manager = ZhihuLoginManager()
-	login_manager.login()
-
+    login_manager = ZhihuLoginManager()
+    login_manager.set_login_method('cookie')
+    login_manager.login()
+    res = login_manager.session.get("http://www.zhihu.com")
+    print(res.text)
--- login_manager_base.py
+++ login_manager_base.py
@@ -8,25 +8,25 @@ import abc
 
 
 class LoginManagerBase:
-	__metaclass__ = abc.ABCMeta
+    __metaclass__ = abc.ABCMeta
 
-	def __init__(self):
-		self.url_root = None
-		self.url_start = None
-		self.login = lambda: (_ for _ in ()).throw(Exception("LoginManager.login not define"))
+    def __init__(self):
+        self.url_root = None
+        self.url_start = None
+        self.login = lambda: (_ for _ in ()).throw(Exception("LoginManager.login not define"))
 
-	@abc.abstractmethod
-	def _login_by_post(self):
-		pass
+    @abc.abstractmethod
+    def _login_by_post(self):
+        pass
 
-	@abc.abstractmethod
-	def _login_by_cookie(self):
-		pass
+    @abc.abstractmethod
+    def _login_by_cookie(self):
+        pass
 
-	def set_login_method(self, method):
-		if method == 'cookie':
-			self.login = self._login_by_cookie
-		elif method == 'post':
-			self.login = self._login_by_post
-		else:
-			raise Exception("set_login_method invalid method")
+    def set_login_method(self, method):
+        if method == 'cookie':
+            self.login = self._login_by_cookie
+        elif method == 'post':
+            self.login = self._login_by_post
+        else:
+            raise Exception("set_login_method invalid method")
